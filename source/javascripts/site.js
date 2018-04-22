94947e3321a9221f381aa0443a6cf06276ca16e4
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-04-22 21:42:30 +0200

    added javasctipt and animation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,16 @@
-// This is where it all goes :)
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let resume = document.getElementById("resume");
+
+
+    resume.addEventListener("mouseover", function( event ) {
+        resume.className += " shake";
+
+        setTimeout(function() {
+            resume.classList.remove("pulse");
+            resume.classList.remove("shake");
+        }, 500);
+    }, false);
+
+});
